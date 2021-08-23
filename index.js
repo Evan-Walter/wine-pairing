1e7cdc129f5f471f72aa6748dfebe776f51e09a1
Evan <ewpw5678@gmail.com>
2021-08-22 18:39:52 -0700

    modified comments

--- index.js
+++ index.js
@@ -1,34 +1,11 @@
-//------------------------------------------------------------------------------
-// Variables for development without api requests
-//------------------------------------------------------------------------------
-// var resExample = {
-//   pairedWines: ["merlot", "cabernet sauvignon", "pinot noir"],
-//   pairingText:
-//     "Merlot, Cabernet Sauvignon, and Pinot Noir are great choices for Steak. After all, beef and red wine are a classic combination. Generally, leaner steaks go well with light or medium-bodied reds, such as pinot noir or merlot, while fattier steaks can handle a bold red, such as cabernet sauvingnon. The Darioush Signature Merlot with a 4.2 out of 5 star rating seems like a good match. It costs about 69 dollars per bottle.",
-//   productMatches: [
-//     {
-//       id: 445574,
-//       title: "Darioush Signature Merlot",
-//       description:
-//         "The moderate 2008 growing season yielded small amounts of mature, highly-concentrated Merlot. The resulting wine offers a profile of dense, expressive dark fruit– black raspberry, cherry and dried fig supported by a tight core of dark cocoa, licorice and cola. Sweet, succulent tannins add textural complexity, while a beautiful, long finish reveals notes of rosemary, cinnamon and graham cracker. Blend: 90% Merlot, 5% Cabernet Sauvignon, 5% Cabernet Franc",
-//       price: "$68.99",
-//       imageUrl: "https://spoonacular.com/productImages/445574-312x231.jpg",
-//       averageRating: 0.8400000000000001,
-//       ratingCount: 5.0,
-//       score: 0.7775000000000001,
-//       link: "https://click.linksynergy.com/deeplink?id=*QCiIS6t4gA&mid=2025&murl=https%3A%2F%2Fwww.wine.com%2Fproduct%2Fdarioush-signature-merlot-2008%2F167005",
-//     },
-//   ],
-// };
+/**
+ * Evan Walter
+ * This program takes a search parameter and retrieves corresponding data from
+ * an api to display on the page.
+ */
 
-// var failureExample = {
-//   status: "failure",
-//   message: "Could not find a wine pairing for seak",
-// };
+// --- Search Form Event Listener ---
 
-//------------------------------------------------------------------------------
-// Search Form Event Listener
-//------------------------------------------------------------------------------
 var searchForm = document.querySelector("#search-form");
 
 searchForm.addEventListener("submit", function (e) {
@@ -41,7 +18,6 @@ searchForm.addEventListener("submit", function (e) {
     if (this.readyState == 4 && this.status == 200) {
       var res = JSON.parse(xhttp.responseText);
       console.log(res);
-      // var res = resExample; // for development without api requests
 
       var searchResultsContainer = document.getElementsByClassName(
         "search-results-container"
@@ -53,7 +29,7 @@ searchForm.addEventListener("submit", function (e) {
       // Check for invalid/valid search terms
       if (res.status === "failure") {
         var failMsg = document.createElement("h4");
-        failMsg.innerHTML = res.message.replace(textValue, `"${textValue}"`); // add quotes around search term in result
+        failMsg.innerHTML = res.message.replace(textValue, `"${textValue}"`);
         searchResultsContainer.appendChild(failMsg);
       } else {
         // Paired Wines Result
@@ -108,9 +84,8 @@ searchForm.addEventListener("submit", function (e) {
   xhttp.send();
 });
 
-//------------------------------------------------------------------------------
-// Helpers
-//------------------------------------------------------------------------------
+// --- Helpers ---
+
 // Capitalize each word
 function titleize(str) {
   return str.replace(/\w\S*/g, function (txt) {
